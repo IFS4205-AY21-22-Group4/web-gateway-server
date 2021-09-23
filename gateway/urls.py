441798c2ca2b1dce1498e28073540d606eb9513e
isaharon <isabharon@gmail.com>
2021-09-23 16:53:38 +0800

    Update urls.py

--- gateway/urls.py
+++ gateway/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import GatewayListCreate, GatewayRecordCreate
+from .views import GatewayListCreate, GatewayRecordCreate, LoginView
+from knox import views as knox_views
 
 urlpatterns = [
+    path("login/", LoginView.as_view(), name="knox_login"),
+    path("logout/", knox_views.LogoutView.as_view(), name="knox_logout"),
+    path("logoutall/", knox_views.LogoutAllView.as_view(), name="knox_logoutall"),
     path("gateways/", GatewayListCreate.as_view()),
     path("gatewayrecord/add/", GatewayRecordCreate.as_view()),
 ]
