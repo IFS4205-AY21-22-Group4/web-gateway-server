fd6d22a45ffb160571b4f2ddea59a8927a3ad973
isaharon <isabharon@gmail.com>
2021-10-14 11:47:31 +0800

    Update urls
    * Add token endpoint to get partial identity

--- gateway/urls.py
+++ gateway/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from .views import GatewayRecordCreate, LoginView, GatewayList, GatewayDetail
+from .views import (
+    LoginView,
+    GatewayList,
+    GatewayDetail,
+    GatewayRecordCreate,
+    TokenDetail,
+)
 from knox import views as knox_views
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="knox_login"),
     path("logout/", knox_views.LogoutView.as_view(), name="knox_logout"),
-    path("logoutall/", knox_views.LogoutAllView.as_view(), name="knox_logoutall"),
-    path("gatewayrecords/", GatewayRecordCreate.as_view()),
     path("v1/gateways/", GatewayList.as_view()),
     path("v1/gateways/<int:pk>", GatewayDetail.as_view()),
+    path("v1/gatewayrecords/", GatewayRecordCreate.as_view()),
+    path("v1/token/<uuid:token_uuid>", TokenDetail.as_view()),
 ]
